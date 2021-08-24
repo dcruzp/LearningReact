705c9ec27117060c07102dc39a51583b57df9bbf
Daniel <danieldelacruzprieto@gmail.com>
2021-08-24 18:16:49 -0400

    updating for deployer application

--- src/components/Buscaminas.jsx
+++ src/components/Buscaminas.jsx
@@ -3,22 +3,54 @@ import '../styles/buscaminas.css';
 
 
 //enviroment global 
-var rows = 9 ; 
-var columns = 9 ; 
+var rows = 8 ; 
+var columns = 8 ; 
+var amountMines = 20;
 
 let dirrow = [-1,-1,-1,0,1,1,1,0]; 
 let dircol = [-1,0,1,1,1,0,-1,-1];
 let gameover = false ; 
 
+//Auxiliars functions
+function range(start, stop, step) {
+  if (typeof stop == 'undefined') {
+      // one param defined
+      stop = start;
+      start = 0;
+  }
+
+  if (typeof step == 'undefined') {
+      step = 1;
+  }
+
+  if ((step > 0 && start >= stop) || (step < 0 && start <= stop)) {
+      return [];
+  }
+
+  var result = [];
+  for (var i = start; step > 0 ? i < stop : i > stop; i += step) {
+      result.push(i);
+  }
+
+  return result;
+};
+
+
 function Cell (props)
 {
   return (
-    <button className="cell" onClick = {props.onClick}>
+    <button 
+      className="cell" 
+      onClick = {props.onClick}
+      style = {{
+        backgroundColor : props.value.isbloked === true  && props.value.mine === true
+      }}>
       {
         props.value.print()
       }
     </button>
   );
+
 }
 
 class Board extends React.Component{
@@ -31,108 +63,28 @@ class Board extends React.Component{
     );
   }
 
+  renderRow(listindex){
+    return(
+      <div className = "board-row">
+        {
+          listindex.map(x => this.renderCell(x))
+        }
+      </div>
+    )
+  }
+
+  renderAll (listrows){
+    return(
+      listrows.map(x => this.renderRow(range(x*columns , x*columns + columns)))
+    );
+  }
+
   render(){
     return (
       <div>
-        <div className="board-row">
-          {this.renderCell(0)}
-          {this.renderCell(1)}
-          {this.renderCell(2)}
-          {this.renderCell(3)}
-          {this.renderCell(4)}
-          {this.renderCell(5)}
-          {this.renderCell(6)}
-          {this.renderCell(7)}
-          {this.renderCell(8)}
-        </div>
-        <div className="board-row">
-          {this.renderCell(9)}
-          {this.renderCell(10)}
-          {this.renderCell(11)}
-          {this.renderCell(12)}
-          {this.renderCell(13)}
-          {this.renderCell(14)}
-          {this.renderCell(15)}
-          {this.renderCell(16)}
-          {this.renderCell(17)}
-        </div>
-        <div className="board-row">
-          {this.renderCell(18)}
-          {this.renderCell(19)}
-          {this.renderCell(20)}
-          {this.renderCell(21)}
-          {this.renderCell(22)}
-          {this.renderCell(23)}
-          {this.renderCell(24)}
-          {this.renderCell(25)}
-          {this.renderCell(26)}
-        </div>
-        <div className="board-row">
-          {this.renderCell(27)}
-          {this.renderCell(28)}
-          {this.renderCell(29)}
-          {this.renderCell(30)}
-          {this.renderCell(31)}
-          {this.renderCell(32)}
-          {this.renderCell(33)}
-          {this.renderCell(34)}
-          {this.renderCell(35)}
-        </div>
-        <div className="board-row">
-          {this.renderCell(36)}
-          {this.renderCell(37)}
-          {this.renderCell(38)}
-          {this.renderCell(39)}
-          {this.renderCell(40)}
-          {this.renderCell(41)}
-          {this.renderCell(42)}
-          {this.renderCell(43)}
-          {this.renderCell(44)}
-        </div>
-        <div className="board-row">
-          {this.renderCell(45)}
-          {this.renderCell(46)}
-          {this.renderCell(47)}
-          {this.renderCell(48)}
-          {this.renderCell(49)}
-          {this.renderCell(50)}
-          {this.renderCell(51)}
-          {this.renderCell(52)}
-          {this.renderCell(53)}
-        </div>
-        <div className="board-row">
-          {this.renderCell(54)}
-          {this.renderCell(55)}
-          {this.renderCell(56)}
-          {this.renderCell(57)}
-          {this.renderCell(58)}
-          {this.renderCell(59)}
-          {this.renderCell(60)}
-          {this.renderCell(61)}
-          {this.renderCell(62)}
-        </div>
-        <div className="board-row">
-          {this.renderCell(63)}
-          {this.renderCell(64)}
-          {this.renderCell(65)}
-          {this.renderCell(66)}
-          {this.renderCell(67)}
-          {this.renderCell(68)}
-          {this.renderCell(69)}
-          {this.renderCell(70)}
-          {this.renderCell(71)}
-        </div>
-        <div className="board-row">
-          {this.renderCell(72)}
-          {this.renderCell(73)}
-          {this.renderCell(74)}
-          {this.renderCell(75)}
-          {this.renderCell(76)}
-          {this.renderCell(77)}
-          {this.renderCell(78)}
-          {this.renderCell(79)}
-          {this.renderCell(80)}
-        </div>
+        {
+          this.renderAll(range(rows))
+        }
       </div>
     );
   }
@@ -163,13 +115,12 @@ function CreateMines (){
 
   var squares = new Array(rows * columns).fill(null).map(() => new Casilla(false));
 
-  var total = 40; 
   var i =0 ; 
-  while(i < total){
-    var index = Math.floor(Math.random() *81); 
+  while(i < amountMines){
+    var index = Math.floor(Math.random() *(rows*columns)); 
     if (squares[index].mine === true) continue; 
     squares[index].mine = true; 
-    squares[index].isbloked = false ; 
+    // squares[index].isbloked = false ; 
     i++; 
   }
 
