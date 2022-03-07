e3369281d2d600ffed4d6a6f4ac2e4b1fe900fcf
dcruzp <danieldelacruzprieto@gmail.com>
2022-03-07 01:14:50 -0600

    adding some charts of examples

--- src/components/Graficos.jsx
+++ src/components/Graficos.jsx
@@ -1,20 +1,94 @@
-import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
-const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400},
-              {name: 'Page B', uv: 300, pv: 2400, amt: 2400},
-              {name: 'Page C', uv: 200, pv: 2400, amt: 2400},
-              {name: 'Page D', uv: 50, pv: 2400, amt: 2400},
-              {name: 'Page E', uv: 200, pv: 2400, amt: 2400},
-              {name: 'Page F', uv: 300, pv: 2400, amt: 2400} ];
+import { AreaChart, Tooltip, Area, CartesianGrid, XAxis, YAxis } from 'recharts';
+// import { LineChart, Line  } from 'recharts';
+
+// const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400},
+//               {name: 'Page B', uv: 300, pv: 2400, amt: 2400},
+//               {name: 'Page C', uv: 200, pv: 2400, amt: 2400},
+//               {name: 'Page D', uv: 50, pv: 2400, amt: 2400},
+//               {name: 'Page E', uv: 200, pv: 2400, amt: 2400},
+//               {name: 'Page F', uv: 300, pv: 2400, amt: 2400} ];
               
-function Grafico ()  {
-  return(
-    <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-    <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-    <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-    <XAxis dataKey="name" />
-    <YAxis />
-  </LineChart>
-  );
+
+const data1 = [
+  {
+    "name": "Page A",
+    "uv": 4000,
+    "pv": 2400,
+    "amt": 2400
+  },
+  {
+    "name": "Page B",
+    "uv": 3000,
+    "pv": 1398,
+    "amt": 2210
+  },
+  {
+    "name": "Page C",
+    "uv": 2000,
+    "pv": 9800,
+    "amt": 2290
+  },
+  {
+    "name": "Page D",
+    "uv": 2780,
+    "pv": 3908,
+    "amt": 2000
+  },
+  {
+    "name": "Page E",
+    "uv": 1890,
+    "pv": 4800,
+    "amt": 2181
+  },
+  {
+    "name": "Page F",
+    "uv": 2390,
+    "pv": 3800,
+    "amt": 2500
+  },
+  {
+    "name": "Page G",
+    "uv": 3490,
+    "pv": 4300,
+    "amt": 2100
+  }
+]
+
+
+// function Grafico ()  {
+//   return(
+//     <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+//     <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+//     <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+//     <XAxis dataKey="name" />
+//     <YAxis />
+//   </LineChart>
+//   );
+// }
+
+function GraficoAreaChart(){
+  return (
+    <AreaChart width={730} height={250} data={data1}
+      margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+      <defs>
+        <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+          <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
+          <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
+        </linearGradient>
+        <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+          <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8}/>
+          <stop offset="95%" stopColor="#82ca9d" stopOpacity={0}/>
+        </linearGradient>
+      </defs>
+      <XAxis dataKey="name" />
+      <YAxis />
+      <CartesianGrid strokeDasharray="3 3" />
+      <Tooltip />
+      <Area type="monotone" dataKey="uv" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+      <Area type="monotone" dataKey="pv" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
+    </AreaChart>
+  )
 }
 
-export default Grafico
+// export default Grafico
+export default GraficoAreaChart
